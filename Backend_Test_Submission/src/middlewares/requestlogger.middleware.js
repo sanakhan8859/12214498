0d0f930534649f6c1d4abb0bf28174f5f860b979
Amber Afreen <103463636+sanakhan8859@users.noreply.github.com>
2025-07-14 14:12:15 +0530

    Update requestlogger.middleware.js

--- Backend_Test_Submission/src/middlewares/requestlogger.middleware.js
+++ Backend_Test_Submission/src/middlewares/requestlogger.middleware.js
@@ -1,29 +1,24 @@
-const axios = require('axios');
-
-const BASE_URL = 'http://20.244.56.144/evaluation-service/logs';
-const AUTH_TOKEN = process.env.AFFORDMED_AUTH_TOKEN; 
 
 
-const Log = async (stack, level, logPackage, message) => {
-    try {
-        const response = await axios.post(
-            BASE_URL,
-            {
-                stack,
-                level,
-                package: logPackage,
-                message
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${AUTH_TOKEN}`
-                }
-            }
-        );
-        console.log("Log sent successfully:", response.data.message);
-    } catch (error) {
-        console.error("Failed to send log:", error.message);
+const logAuthMiddleware = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({
+            message: 'Authorization token missing or invalid. Please provide token in "Authorization: Bearer <token>" format.'
+        });
     }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            message: 'Bearer token not provided.'
+        });
+    }
+
+    next();
 };
 
-module.exports = { Log };
+module.exports = {
+    logAuthMiddleware
+};
